93ec053bddb2af83ae65c0aeeccdbe7d976355d9
Lucio <lucio_sb@yahoo.com.ar>
2023-09-03 05:50:08 -0300

    v0.8.0 - Search sidebar mockup + styling (#4)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-end p-4 bg-slate-200 text-slate-800 absolute bottom-0 w-full">
-        <p className="text-lg">
-            by&nbsp;
-            <a
-                href="https://github.com/l-s-b"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="cursor-pointer hover:text-black hover:font-bold duration-500"
-            >
-                l-s-b
-            </a>
-        </p>
+    <footer className="flex justify-end p-4 bg-slate-200 text-slate-800 fixed z-40 bottom-0 w-full vanish">
+      <p className="text-lg">
+        by&nbsp;
+        <a
+          href="https://github.com/l-s-b"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-black hover:font-bold duration-500"
+        >
+          l-s-b
+        </a>
+      </p>
     </footer>
-  )
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="flex items-center p-4 bg-slate-200 text-slate-800">
+    <header className="fixed z-40 top-0 w-full flex items-center p-4 bg-slate-200 text-slate-800 vanish">
         <h1 className="text-center text-xl">My GitHub Commits</h1>
     </header>
   )
